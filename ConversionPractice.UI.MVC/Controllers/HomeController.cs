d4c1ed5a9333816479ba841836d40a9b7f7419d5
Caleb Mitchell <calmitch14@gmail.com>
2021-08-24 14:07:56 -0500

    Initial Commit

--- ConversionPractice.UI.MVC/Controllers/HomeController.cs
+++ ConversionPractice.UI.MVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ConversionPractice.UI.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About";
 
             return View();
         }
